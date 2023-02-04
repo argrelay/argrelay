9aa1665df67fa5a862d174ae91ac7c9e59749456
Alexey Pakseykin <alexey.pakseykin@si.email.gs.com>
2023-02-05 02:15:02 +0800

    Bump up to next version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ extras_require = {
 
 setuptools.setup(
     name = "argrelay",
-    version = "0.0.0.dev13",
+    version = "0.0.0.dev14",
     author = "uvsmtid",
     author_email = "uvsmtid@gmail.com",
     description = "TODO",
