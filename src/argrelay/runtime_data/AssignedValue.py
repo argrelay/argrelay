6401d0dc93629e3676440c9e05fd2f309dad3b58
Alexey Pakseykin <alexey.pakseykin@si.email.gs.com>
2023-02-04 22:26:22 +0800

    Clean up "coordinate" usage

--- src/argrelay/runtime_data/AssignedValue.py
+++ src/argrelay/runtime_data/AssignedValue.py
@@ -3,8 +3,6 @@ from dataclasses import dataclass
 from argrelay.enum_desc.ArgSource import ArgSource
 
 
-# TODO: Forget about coords everywhere (rename `coord` to something else unless it's just an analogy).
-#       Use args for input.
 @dataclass
 class AssignedValue:
     arg_value: str
