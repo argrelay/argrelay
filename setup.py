cd681a78d15787be73f8d0b638311bdaad2cc6f2
Alexey Pakseykin <alexey.pakseykin@si.email.gs.com>
2023-02-16 23:47:19 +0800

    Bump version number

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras_require = {
 
 setuptools.setup(
     name = "argrelay",
-    version = "0.0.0.dev18",
+    version = "0.0.0.dev19",
     author = "uvsmtid",
     author_email = "uvsmtid@gmail.com",
     description = "Bash Tab-completion (data) server",
