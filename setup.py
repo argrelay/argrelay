6e884d7687f2bf7743e7faf0e6b83c49810a9134
Alexey Pakseykin <alexey.pakseykin@si.email.gs.com>
2023-12-23 16:04:56 +0800

    Update GUI client banner

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def list_dir(
 setuptools.setup(
     name = "argrelay",
     # See `docs/dev_notes/version_format.md`:
-    version = "0.5.2.dev1",
+    version = "0.5.2.dev2",
     author = "uvsmtid",
     author_email = "uvsmtid@gmail.com",
     description = "Tab-completion & data search server - total recall",
