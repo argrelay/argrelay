907e4e948ad286e78249fa488ecc4a31c55c19d7
mrchoo <dev@halfdata.com>
2024-01-20 09:55:37 +0100

    Improve spinner

--- src/argrelay/relay_client/proc_parent.py
+++ src/argrelay/relay_client/proc_parent.py
@@ -1,28 +1,60 @@
 import os
+import signal
 import time
 from random import randrange
 
+from argrelay.client_spec.ShellContext import ShellContext
 from argrelay.enum_desc.TermColor import TermColor
 from argrelay.misc_helper_common import eprint
 
 # Use regular Tab indent size:
 spinner_length: int = 4
 
+# Noticeable threshold is around 200 ms:
+initial_sleep_sec: float = 0.2
+
+# Rotate spinner state every:
+spinner_frame_sec: float = 0.1
+
+def signal_handler(signal_number, signal_frame):
+    if signal_number == signal.SIGCHLD:
+        # The child exited:
+        pass
+
 
 def spin_wait_for_child(
     child_pid: int,
+    shell_ctx: ShellContext,
 ):
     """
     Display spinner while child request is running.
     """
+
+    signal.signal(signal.SIGCHLD, signal_handler)
+    # Prevent spinner on instant replies - have an initial sleep first
+    # before drawing spinner for the first time
+    # (this sleep will exit prematurely if the child exits):
+    time.sleep(initial_sleep_sec)
+    if not is_child_running(child_pid):
+        return
+
+    # We waited enough - start spinner:
     pending_cursor = generate_pending_cursor()
     while is_child_running(child_pid):
+        # Print spinner state chars:
         eprint(next(pending_cursor), end = "", flush = True)
-        time.sleep(0.1)
+        time.sleep(spinner_frame_sec)
+        # Step backward:
         eprint("\b" * spinner_length, end = "")
 
-    # Clear spinner state chars:
-    eprint(" " * spinner_length, end = "")
+    # Overwrite spinner state chars by expected content from the prev command line
+    # (padded with spaces, if beyond the end of the line):
+    first_cpos = shell_ctx.cursor_cpos
+    last_cpos = shell_ctx.cursor_cpos + spinner_length
+    line_portion = shell_ctx.command_line[first_cpos:last_cpos]
+    pad_length = spinner_length - len(line_portion)
+    eprint(line_portion + " " * pad_length, end = "")
+    # Step backward:
     eprint("\b" * spinner_length, end = "", flush = True)
 
 
